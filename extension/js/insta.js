53865b4ff9a7454597697a04c63038a2a6ef19dc
Paddy O'Reilly <github@smallroomstudios.net>
2012-11-19 21:51:18 +0000

    Restructured the javascript. Added layout and feed prefs. Rudamentary options

--- extension/js/insta.js
+++ extension/js/insta.js
@@ -1,55 +1,80 @@
-var widgetInit =  widget.preferences.authEndpoint; // HACK 
-var prefs = widget.preferences;
-var authDialog = prefs.authEndpoint+
-  '?client_id='+prefs.clientId
-  +'&redirect_uri='+prefs.callback
-  +'&response_type=token';
-
-// Set Loading text
-opera.contexts.speeddial.title="Loading...";
-  
-opera.contexts.speeddial.url = authDialog;
-
-opera.extension.onmessage = function(e) {
-  var m = e.data;
-  switch(m.action){
-    case 'success':
-      widget.preferences.token = m.access_token;
-      printGrams();
-      break;
-    case 'error':
-      console.log('auth error');
-      document.body.innerHTML = 'auth error';
-      break;
-  }
-}
-
-function printGrams(){
-  if(prefs.token=='undefined' || prefs.token==''){
-    return false;
-  }
-
-  var api = new XMLHttpRequest();
-  var api_url = widget.preferences.apiEndpoint + widget.preferences.feedPath + widget.preferences.token;
-  api.open( 'GET', api_url, true );
-  api.onreadystatechange=function(e){
-    if( this.readyState!==4 ){
-      return false;
-    };
-
+var test = widget.preferences.callback;
+var insta = {
+  prefs : widget.preferences,
+  photos:{},
+  grid : function(photos){
     document.getElementById('frames').innerHTML='';
-    var feed = JSON.parse(this.response);
-    for(var i=0;feed.data.length>i;i++){
-      var url = feed.data[i].images.thumbnail.url;
+    for(var i=0;photos.length>i;i++){
+      var url = photos[i].images.thumbnail.url;
       var img = document.createElement("IMG");
       img.className="grid";
       img.src = url;
       document.getElementById('frames').appendChild(img);
     }
+
     document.body.className='';
-    opera.contexts.speeddial.title="OMG Muffins @ Cafe Bliss";
+    opera.contexts.speeddial.title="OMG Muffins @ Cafe Bliss"; 
+  },
+
+  cron : function(){
+    if(this.prefs.token=='undefined' || this.prefs.token==''){
+      return false;
+    }
+
+    // Endpoint
+    switch(insta.prefs.feed){
+      default:
+        var api_url = insta.prefs.apiEndpoint + insta.prefs.feedPath + insta.prefs.token; 
+        break;
+    }
+
+    // Layout
+    switch(insta.prefs.layout){
+      case 'grid':
+        insta.get(api_url, this.grid);
+        break;
+    }
+    setTimeout(insta.cron, insta.prefs.updateInterval);
+  },
+
+  get : function(url, callback){
+    var api = new XMLHttpRequest();
+    api.open( 'GET', url, true );
+    api.onreadystatechange=function(e){
+      if(this.readyState===4){
+        var feed = JSON.parse(this.response);
+        callback(feed.data);
+      }
+    }
+    api.send(null);
+  },
+
+  message : function(e){
+    var m = e.data;
+    switch(m.action){
+      case 'success':
+        insta.prefs.token = m.access_token;
+        insta.grid();
+        break;
+      case 'error':
+        console.log('auth error');
+        document.body.innerHTML = 'auth error';
+        break;
+    }
+  },
+
+  init : function(){
+    insta.authDialog = insta.prefs.authEndpoint+
+      '?client_id='+insta.prefs.clientId
+      +'&redirect_uri='+insta.prefs.callback
+      +'&response_type=token';
+
+    // Set Loading text
+    opera.contexts.speeddial.title="Loading...";
+    opera.contexts.speeddial.url = this.authDialog;
+    opera.extension.onmessage = insta.message;
+
+    insta.cron();
   }
-  api.send(null);
 }
-printGrams();
-setInterval(printGrams, prefs.updateInterval);
+window.addEventListener("load", insta.init, false); insta
